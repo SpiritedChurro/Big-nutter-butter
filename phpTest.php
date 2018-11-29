6768f691bb51ed48d7ab2e7a4c5f31d876707d56
RXZ <44505898+SpiritedChurro@users.noreply.github.com>
2018-11-29 09:14:52 -0800

    Add files via upload

---
+++ phpTest.php
@@ -0,0 +1,12 @@
+<?php
+$host = "127.0.0.01";
+$user = "rxzucc";
+$pass = "";
+$db = "c9";
+$port = 3306;
+ $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+ $query = "SELECT * FROM users";
+ $result = mysqli_query($connection, $query);
+ while ($row = mysqli_fetch_assoc($result)) {
+        echo "The ID is: " . $row['id'] . " and the Username is: " . $row['username'];
+    }
